b8ab95c487b3d53b3ae2ee1f560ec76d26f7470e
Vladimir Dudin <mr.wwwf@gmail.com>
2023-07-28 19:46:38 +0300

    Tested all methods.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Basket basket = new Basket();
         basket.add("Молоко", 40, 1, 0.93);
-        basket.add("Картофель", 5, 12, 1.43);
+        basket.add("Картофель", 8, 12, 1.43);
+        basket.add("Шарики",5,5);
+        basket.add("Пакет", 2);
+
         basket.print("Ваша корзина");
         basket.getTotalWeight();
         basket.getTotalPrice();
